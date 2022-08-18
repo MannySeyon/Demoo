5720178c93d9e5776612061ef2d59cafb7bd6c5f
Ruhamakebede <ruhama.kebede@summitoic.edu>
2022-08-18 13:59:38 -0500

    test22

--- src/demoo.java
+++ src/demoo.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class demoo {
     public static void main(String[] args) {
         System.out.println(" this is test1");
@@ -5,5 +7,6 @@ public class demoo {
         System.out.println("apple123");
         System.out.println("Mannyson");
         System.out.println("Mannyyyy");
+        System.out.println("this is test 2 ");
     }
 }
