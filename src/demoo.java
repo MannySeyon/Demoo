636741d85a7d203c0f47e6bad5d28989038ba232
w3schools-test <test@w3schools.com>
2022-08-18 13:55:29 -0500

    pandora v.4

--- src/demoo.java
+++ src/demoo.java
@@ -2,7 +2,7 @@ public class demoo {
     public static void main(String[] args) {
         System.out.println(" this is test1");
         System.out.println("again");
-        System.out.println("Pandora testing");
+        System.out.println("Pandora");
         System.out.println("Mannyson");
         System.out.println("Mannyyyy");
     }
