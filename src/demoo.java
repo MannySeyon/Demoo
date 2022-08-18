9d1db1f019772070af5fd0755e1f465cd92f3286
Emmanuel Seyon <noyysey@gmail.com>
2022-08-18 13:29:55 -0500

    Mannys1

--- src/demoo.java
+++ src/demoo.java
@@ -2,6 +2,6 @@ public class demoo {
     public static void main(String[] args) {
         System.out.println(" this is test1");
         System.out.println("again");
-
+        System.out.println("Mannyson");
     }
 }
